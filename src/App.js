53c2580b68bbf5da26024930a068ae3f05399661
Umeitu <umeitupelpuo@gmail.com>
2022-03-17 13:58:01 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import Todo from './components/Todo';
 
-function App() {
+const App = () => {
+  let title = "my first react app";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <NavBar title={title}/>
+    <main>
+    <Todo title="learning react"datetime= " 23rd March 2022"/>
+    <Todo title="premest training"datetime= "2nd january 2022"/>
+    <Todo title="going to market" datetime="3rd April 2022"/>
+    <Todo title="writing exams"datetime="18th December 2022" / >
+    <Todo title="Depreciating cedis"datetime="4th october 2022"/>
+    </main>
+  </div>
   );
 }
 
